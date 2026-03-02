8831eed69ffcd7ee6ca9a63dc6b1c6d83aa52520
Dimitri <dimitri_ham@hotmail.com>
2026-03-02 13:34:22 +0100

    Update Web Scapping.py
    Le programme fonctionne pour n'importe quel site web avec la lib Newspaper3k ... sauf la partie NLP pour avoir le résumé de l'article.
    => 4 lignes qui débutent par #ERROR#   
    
    Prochaine étape : 
        # Une fois validé il faudra faire une sauvegarde des articles et, si on recharge la même liste de sites on ne fera qu'un delta avec newspaper3k
        # Pour cela il faudra que je sauvegarde mes résultats dans un fichier qui sera itéré si on recharge les derniers mots clés ET les derniers sites
        # Il semble même possible d'archiver dans un fichier la liste des articles déjà extraits d'un site d'informations pour ne récupérer que les nouveaux articles 
        #       Source : Article caching dans https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html#extracting-articles
        #       Outil : memoize
        # Ensuite j'afficherai les résultats de la recherche via mon big dico et une boucle FOR dédiée

--- Web Scapping.py
+++ Web Scapping.py
@@ -3,7 +3,10 @@
 # Installation du package BeautifullSoup pour parser du HTML : Python -m pip install beautifulsoup4
 # Documentation Beautifull Soup : https://www.crummy.com/software/BeautifulSoup/
 # Installation du package Newspaper3k pour identifier les articles sur des sites d'information : Python -m pip install newspaper3k
-# Documentation : https://scrapeops.io/python-web-scraping-playbook/newspaper3k/ et https://pypi.org/project/newspaper3k/ 
+# Il faut aussi installer le module lxml.html.clean qui sert à nettoyer les pages HTML avec la commande : Python -m pip install lxml_html_clean
+# Documentation : https://scrapeops.io/python-web-scraping-playbook/newspaper3k/ et https://newspaper.readthedocs.io/en/latest/ et quickstrat https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html#extracting-articles 
+# Documentation : https://lxml.de/ 
+# ======>>>>> ATTENTION A CREER UN ENVIRONNEMENT VIRTUEL POUR CES DEPENDANCES !!!!!
 
 # FICHIERS
 # Pour la saisie des mots clés et sites les fichiers utilisés pour stocker les données saisies sont : 
@@ -29,10 +32,12 @@
 # Cette structure correspond à une BdD SQL avec une table principale liste_articles et une table secondaire site_infos dont site est la clé étrangère et qui contient toutes les métadonnées de chaque site
 # article_tag_url : url
 # article_tag_title : titre
-# article_tag_sumup : résumé
+# article_tag_sumup : résumé généré par NLP de naewspaper3k
+# article_tag_text : contenu textuel
 # article_tag_date : date
 # article_tag_keywords : liste des mots clés
 
+import newspaper
 import requests
 from bs4 import BeautifulSoup
 
@@ -106,31 +111,30 @@ print("\n")
 dico_articles = {}
 
 for saisie in sites: #On parcourt la liste des url saisies par l'opérateur pour les scrapper
-    # Attention, pour le moment, les balises utilisées pour la recherche de données sur la page correspondent à l'url ci-dessous (à généraliser) :
-    # 'https://www.navalnews.com/category/naval-news/'
-
-    resultat = requests.get(saisie) #On récupère la réponse http du site web mais on ne peut pas l'interpréter
-    soup = BeautifulSoup(resultat.content,"html.parser") #On transforme le code html en chaine de caractères avec le parser html.parser inclus dans Python (sinon il faut installer une dépendance dédiée)
-    article_list = soup.select('article') #On crée la liste de toutes les balises "article" du site
-
+    resultat = newspaper.build(saisie) #On crée une liste d'objets avec la librairie newspaper3k. Ces objets sont de type "Article" et chacun contient un article entier prêt à être téléchargé et parsé
+    article_list = resultat.articles #On crée la liste des objets "article" présents dans le site de news...plus tard on pourra regarder par rapport à un fichier d'archive juste les nouveaux articles si on charge une liste exitante de sites
     for article_tag in article_list:
         #Inititiation du dictionnaire de métadonnées vierge
         dico_metadonnees = {'titre': "x", 'résumé': "x", 'date': "x", 'texte': "x", 'mots clés': []}
         # Extraction des métadonnées
-        article_tag_url = article_tag.find('a').get('href') #On récupère le lien de l'article après la balise <a dans l'id href
-        article_tag_title = article_tag.find('h3').get_text().strip() #On récupère le titre de l'article après la balise h3 et le strip retire les espaces vides de début et fin
-        article_tag_sumup = article_tag.find('p').get_text().strip() #On récupère le résumé de l'article après la balise p
-        article_tag_date = article_tag.find('span').get_text().strip() #On récupère la date de l'article après la balise span
+        article_tag.download() #On télécharge le code HTML brut de l'article
+        article_tag.parse() #On parse le code HTML brut de l'article
+        article_tag_url = article_tag.url #On récupère le lien de l'article après la balise <a dans l'id href
+        article_tag_title = article_tag.title #On récupère le titre de l'article après la balise h3 et le strip retire les espaces vides de début et fin
+        article_tag_text = article_tag.text #On récupère le résumé de l'article après la balise p
+        article_tag_date = article_tag.publish_date #On récupère la date de l'article après la balise span
         #Recherche des mots-clés
         for mot in motscle:
-            if mot.lower() in str(soup).lower():
-                #Si le mot clé est présent dans le code hltm, on l'ajoute aux mots-clés trouvés
+            if mot.lower() in article_tag_text.lower():
+                #Si le mot clé est présent dans le texte de l'article (on ne regarde pas le titre), on l'ajoute aux mots-clés trouvés
                 #Tout est passé en lower case pour ne pas être sensible à la casse
                 dico_metadonnees["mots clés"].append(mot)
-            if mot.lower() in str(soup).lower() and dico_metadonnees["titre"]=="x":
+            if mot.lower() in article_tag_text.lower() and dico_metadonnees["titre"]=="x":
                 #Si c'est le premier mot clé trouvé sur cet article, on ajoute les métadonnées communes à tout l'article
+                #article_tag.nlp() #On ne fait passer l'IA qu'une fois que le mot clé est identifié dans l'article pour limiter la consommation de ressources
+                #article_tag_sumup = article_tag.summary
                 dico_metadonnees["titre"]=article_tag_title
-                dico_metadonnees["résumé"]=article_tag_sumup
+                #dico_metadonnees["résumé"]=article_tag_sumup
                 dico_metadonnees["date"]=article_tag_date
         #Si on a trouvé au moins un mot clé dans l'article alors on le référence dans notre dico de résultats
         if len(dico_metadonnees["mots clés"]) > 0:
@@ -138,7 +142,7 @@ for saisie in sites: #On parcourt la liste des url saisies par l'opérateur pour
             print("Sur le site", article_tag_url, "nous avons trouvé :")
             print("- les mots clés suivants :", dico_metadonnees["mots clés"])
             print("- le titre de l'article est :", article_tag_title)
-            print("- le résumé de l'article est :", article_tag_sumup)
+            #print("- le résumé de l'article est :", article_tag_sumup)
             print("- la date de l'article est :", article_tag_date)
             print("\n")
     
@@ -151,12 +155,16 @@ if dico_articles == {}:
 
 
     # J'en suis là : 
-    # J'ai à jour mon big dico d'articles pour tous les articles du site de la première boucle (qui ne marche pour le moment que pour un site)
-    # Faut maintenant que je vérifie si ça itère pour tous les sites 'saisie' de la liste 'sites'.
-    # Tester avec https://www.naval-group.com/fr et https://www.navalnews.com/category/naval-news/
-    # J'ai l'impression qu'il faut que je trouve toutes les balises <a puis que je remonte au-dessus d'elles pour trouver titre, résumé et date... 
-    # Mais pas sûr qu'on ait ces infos sur tous les sites donc il faudra peut être que j'explore l'URL associée à la balise <a à la recherche des mots clés et autres infos
+    # J'ai utilisé le package Newspaper3k mais 
+    # Le module NLP ne fonctionne pas donc j'ai mis en commentaire les lignes associées 
+
+    # Une fois validé il faudra faire une sauvegarde des articles et, si on recharge la même liste de sites on ne fera qu'un delta avec newspaper3k
+    # Pour cela il faudra que je sauvegarde mes résultats dans un fichier qui sera itéré si on recharge les derniers mots clés ET les derniers sites
+    # Il semble même possible d'archiver dans un fichier la liste des articles déjà extraits d'un site d'informations pour ne récupérer que les nouveaux articles 
+    #       Source : Article caching dans https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html#extracting-articles
+    #       Outil : memoize
     # Ensuite j'afficherai les résultats de la recherche via mon big dico et une boucle FOR dédiée
+
 
 
         
